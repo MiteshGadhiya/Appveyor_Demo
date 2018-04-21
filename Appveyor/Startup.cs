7d53d14df2a91fc6d8665a295c032c012084bb65
Mitesh Gadhiya <m.thakkars@gmail.com>
2018-04-21 13:29:53 +0530

    Revert "Revert "Initial Commit""
    This reverts commit 6e5bd3a4ff0f376b9adc2b091827630fd9e90b49.

---
+++ Appveyor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Appveyor.Startup))]
+namespace Appveyor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
